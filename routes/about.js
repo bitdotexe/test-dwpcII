7da6f8ac2125b9ba7b8af5e80669b96393b01fc5
bit <uriel09890@gmail.com>
2023-09-13 00:17:48 -0600

    Beutifying the code

--- routes/about.js
+++ routes/about.js
@@ -5,7 +5,7 @@ router.get("/", function (req, res, next) {
   res.send("Escriba /about/tec en el navegador");
 });
 
-// Lista de URLs de imágenes aleatorias
+// images array
 const randomImages = [
   "https://gamadero.tecnm.mx/assets/files/main/img/pleca_tenm%20-%20copia.png",
   "https://gamadero.tecnm.mx/assets/files/main/img/inicioitgam.jpg",
@@ -13,20 +13,34 @@ const randomImages = [
   "https://sii.gamadero2.tecnm.mx/web/image/22361/20190201_124553_Film2.jpg",
 ];
 
+// Data object
+const aboutTec = {
+  name: "Instituto Tecnológico de Gustavo A. Madero",
+  description:
+    "El Instituto Tecnológico de Gustavo A. Madero es una institución educativa ubicada en la alcaldía Gustavo A. Madero de la Ciudad de México, México. Este instituto ofrece una variedad de programas académicos en áreas como ingeniería, tecnología y ciencias aplicadas. Su enfoque principal es proporcionar una educación de calidad en estas disciplinas, promoviendo la formación de profesionales competentes en el ámbito tecnológico. El Instituto Tecnológico de Gustavo A. Madero también puede estar involucrado en proyectos de investigación y colaboraciones con la comunidad local para contribuir al desarrollo tecnológico y económico de la región.",
+  mission:
+    "La misión del Instituto Tecnológico de Gustavo A. Madero es proporcionar educación superior de calidad en áreas tecnológicas y científicas, con un enfoque en la formación de profesionales altamente capacitados y éticos. Busca promover la investigación, la innovación y la colaboración con la comunidad, contribuyendo al desarrollo económico y tecnológico de la región y del país. Su objetivo principal es preparar a los estudiantes para enfrentar los desafíos del mundo moderno y ser agentes de cambio positivo en sus respectivas áreas de estudio y en la sociedad en general.",
+  values:
+    "El Instituto Tecnológico de Gustavo A. Madero se fundamenta en una sólida ética de calidad educativa, promoviendo la excelencia académica, la integridad y la responsabilidad. Prioriza la innovación y la adaptabilidad, fomentando la creatividad y la colaboración, mientras valora la diversidad y la igualdad de oportunidades. Comprometido con el desarrollo sustentable y la vinculación activa con la comunidad, busca contribuir al progreso socioeconómico y tecnológico, reflejando así su compromiso con una educación superior de calidad y valores sólidos.",
+};
+
 /* GET about/tec. */
 router.get("/tec", (_, res) => {
-  // Selecciona una imagen aleatoria de la lista
+  // RandomImage
   const randomImage =
     randomImages[Math.floor(Math.random() * randomImages.length)];
-
   res.render("about", {
-    name: "Instituto Tecnológico de Gustavo A. Madero",
-    description:
-      "El Instituto Tecnológico de Gustavo A. Madero es una institución educativa ubicada en la alcaldía Gustavo A. Madero de la Ciudad de México, México. Este instituto ofrece una variedad de programas académicos en áreas como ingeniería, tecnología y ciencias aplicadas. Su enfoque principal es proporcionar una educación de calidad en estas disciplinas, promoviendo la formación de profesionales competentes en el ámbito tecnológico. El Instituto Tecnológico de Gustavo A. Madero también puede estar involucrado en proyectos de investigación y colaboraciones con la comunidad local para contribuir al desarrollo tecnológico y económico de la región.",
-    mission:
-      "La misión del Instituto Tecnológico de Gustavo A. Madero es proporcionar educación superior de calidad en áreas tecnológicas y científicas, con un enfoque en la formación de profesionales altamente capacitados y éticos. Busca promover la investigación, la innovación y la colaboración con la comunidad, contribuyendo al desarrollo económico y tecnológico de la región y del país. Su objetivo principal es preparar a los estudiantes para enfrentar los desafíos del mundo moderno y ser agentes de cambio positivo en sus respectivas áreas de estudio y en la sociedad en general.",
-    values:
-      "El Instituto Tecnológico de Gustavo A. Madero se fundamenta en una sólida ética de calidad educativa, promoviendo la excelencia académica, la integridad y la responsabilidad. Prioriza la innovación y la adaptabilidad, fomentando la creatividad y la colaboración, mientras valora la diversidad y la igualdad de oportunidades. Comprometido con el desarrollo sustentable y la vinculación activa con la comunidad, busca contribuir al progreso socioeconómico y tecnológico, reflejando así su compromiso con una educación superior de calidad y valores sólidos.",
+    // SpreadOperator
+    ...aboutTec,
+    image: randomImage,
+  });
+});
+
+router.get("/api/tec", function (_, res, _) {
+  const randomImage =
+    randomImages[Math.floor(Math.random() * randomImages.length)];
+  res.send({
+    ...aboutTec,
     image: randomImage,
   });
 });
