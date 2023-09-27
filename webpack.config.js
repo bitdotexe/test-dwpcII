1e4ec8146c7f03390ae5af4b569a392f2214202a
John Morales <johnta2610@hotmail.com>
2023-09-27 00:57:43 +0000

    🌟 Css loader Done!!

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,37 @@
-// Notas importanes
-// El archivo de configuración debe usar ES5
+// Important notes
+// 🚨 Configuration file must use ES5 not ES6
+// that's why you will see "requires" not "imports"
 
-// Importar un administrador de rutas de archivos
+// Importing an file routing manager
 const path = require("path");
+// Importing plugin
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-// Exportamos un objeto de configuración
-// que sera usado por webpack
+// We export a configuration object
+// that will be used by webpack
 module.exports = {
-  // 0. Estableciendo el modo produccion
-  mode: "production",
-  // 1. El archivo de entrada o indexador
+  // 1. The entry file from which
+  // it will contain all the definitions to package
   entry: "./client/index.js",
-  // 2. Especificar el archivo de salida
+  // 2. Specify the output file
+  // Here it is detailed where the file will be
+  // final packaged.
   output: {
-    // 2.1 Ruta absoluta de salida
+    // 2.1 Absolute output path
+    // Note that it is being placed in the directory
+    // of the project's static files
     path: path.resolve(__dirname, "public"),
-    // 2.2 Nombre del archivo de salida
+    // 2.2 Output file name
     filename: "bundle.js",
   },
-  // Agregando un modulo a webpack
+  // Adding a module to webpack
   module: {
     rules: [
       {
+        // This section stablishes
+        // what rules to apply to ".js" files
         test: /\.js$/,
+        // We Dont want to transpile any kind of modules
         exclude: /(node_modules|bower_components)/,
         use: [
           {
@@ -43,6 +52,16 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Archivo css de salida
+      filename: "styles/app.css",
+    }),
+  ],
 };
