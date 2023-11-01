c9df5c63b5ed9f09067430527005f6220dac072a
John Morales <johnta2610@hotmail.com>
2023-11-01 09:56:35 -0600

    ⭐Feat: Compiling model

---
+++ server/domains/project/project.model.js
@@ -0,0 +1,22 @@
+//  Importando Mongoose
+import mongoose from 'mongoose';
+//  Desestructurando la función Schema
+const { Schema } = mongoose;
+//  Construir un Schema
+const ProjectSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+//  Compilando el Schemq para generar el modelo
+export default mongoose.model('project', ProjectSchema);
