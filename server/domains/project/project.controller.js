ea531840ce571f1103e9b555526d348331d65c93
Uriel Torres <uriel09890@gmail.com>
2023-11-14 01:47:30 +0000

    ⭐ Enhance: Adding yup validator. Using YUP and its validator we'll be able to generate errors. If the form has a missing or missing data.

--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -1,3 +1,5 @@
+import log from '../../config/winston';
+
 // Actions methods
 
 // GET '/project/showDashboard'
@@ -11,14 +13,22 @@ const addForm = (req, res) => {
 
 // POST "/project/add"
 const addPost = (req, res) => {
-  // Extrayendo la informacion
-  // del formulario
-  const { name, description } = req.body;
-  // Regresando al cliente la información recabada
-  res.status(200).json({
-    name,
-    description,
-  });
+  // Rescatando la info del formulario
+  const { errorData: validationError } = req;
+  // En caso de haber error
+  // se le informa al cliente
+  if (validationError) {
+    log.info('Se entrega al cliente error de validación de add Project');
+    res.status(422).json(validationError);
+  } else {
+    // En caso de que pase la validación
+    // Se desestructura la información
+    // de la peticion
+    const { validData: project } = req;
+    // Se contesta la información
+    // del proyecto al cliente
+    res.status(200).json(project);
+  }
 };
 
 // Controlador user
--- server/domains/project/project.router.js
+++ server/domains/project/project.router.js
@@ -4,6 +4,11 @@ import { Router } from 'express';
 // Importando el controlador
 import projectController from './project.controller';
 
+// Importando factory de validación
+import ValidateFactory from '../../services/validateFactory';
+// Importando el validador de proyectos
+import projectValidator from './project.validator';
+
 // Creando una isntancia del enrutador
 const router = new Router();
 
@@ -15,7 +20,14 @@ router.get(['/', '/dashboard'], projectController.showDashboard);
 // GET '/project/add'
 router.get(['/add-form', '/add'], projectController.addForm);
 // POST "/project/add"
-router.post('/add', projectController.addPost);
+router.post(
+    '/add',
+    ValidateFactory({
+        schema: projectValidator.projectSchema,
+        getObject: projectValidator.getProject,
+    }),
+    projectController.addPost
+);
 
 // Exporto este tramo de ruta
 export default router;
