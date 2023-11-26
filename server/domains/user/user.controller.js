e9c4aec1ffa0bd8115984aa559d5e77a79b7f0cb
John Morales <johnta2610@hotmail.com>
2023-11-26 12:29:47 -0600

    ⭐Enhancement: Login and session

--- server/domains/user/user.controller.js
+++ server/domains/user/user.controller.js
@@ -1,16 +1,22 @@
-// Actions methods
+import log from '../../config/winston';
+// Action Methods
 
 // GET '/user/login'
 const login = (req, res) => {
-  res.send("🚧 Under Construction '/user/login' 🚧");
+  // Sirve el formulario de login
+  log.info('Se entrega formulario de login');
+  res.render('user/login');
 };
+
 // GET '/user/logout'
 const logout = (req, res) => {
   res.send("🚧 Under Construction '/user/logout' 🚧");
 };
+
 // GET '/user/register'
 const register = (req, res) => {
-  res.send("🚧 Under Construction '/user/register' 🚧");
+  log.info('Se entrega formulario de registro');
+  res.render('user/register');
 };
 
 // Controlador Home
